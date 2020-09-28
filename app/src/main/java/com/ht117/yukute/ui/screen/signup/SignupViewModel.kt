c791686771a00284b365d1614b16200819507104
Juma Allan <allanjuma@gmail.com>
2020-09-28 12:28:58 +0300

    cleaning up linter warnings

--- app/src/main/java/com/ht117/yukute/ui/screen/signup/SignupViewModel.kt
+++ app/src/main/java/com/ht117/yukute/ui/screen/signup/SignupViewModel.kt
@@ -2,5 +2,8 @@ package com.ht117.yukute.ui.screen.signup
 
 import androidx.lifecycle.ViewModel
 
-class SignupViewModel: ViewModel() {
-}
+class SignupViewModel : ViewModel() {
+
+    // TODO - Remove linter warning
+    fun demo() = run { // this will do nothing }
+    }
